ce8f6be54347e04f5180c221247b3d927009b6a1
李成林 <liererer@163.com>
2025-12-25 09:57:51 +0800

    移除无用代码

--- test_models.py
+++
@@ -1,7 +0,0 @@
-from core.models.base import BaseFile
-from core.models.skill import SkillsFile, Skill
-from core.models.point import PointsFile, Point
-
-print(BaseFile.from_dict({}).to_dict())
-print(SkillsFile.from_dict({"skills":[{"id":"1","name":"A","pixel":{"tolerance":12}}]}).to_dict())
-print(PointsFile.from_dict({"points":[{"id":"2","name":"P","x":10,"y":20}]}).to_dict())
